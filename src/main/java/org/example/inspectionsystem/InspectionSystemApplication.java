d45f7ae4bbac740e81a6b49a0760071b6ccb5450
zuozuokan <2074217155@qq.com>
2025-12-23 14:57:33 +0800

    first commit

---
+++ src/main/java/org/example/inspectionsystem/InspectionSystemApplication.java
@@ -0,0 +1,13 @@
+package org.example.inspectionsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InspectionSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InspectionSystemApplication.class, args);
+    }
+
+}
